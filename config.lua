f6e0736f4012373170f6edb966d308ff64c5c23f
mrnimazw <38697745+mrnimazw@users.noreply.github.com>
2018-04-24 19:43:45 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 config = {
-	Channel_Link = 'Https://T.me/GrandTeam', --- محل قرار دادن لینک چنل 
-Sudo_UserName = 'Https://T.me/InitLife', --- محل قرار دادن ایدی سودو 
-Sodo_PmResan = 'Https://T.me/InitLifeBot', --- محل قرار دادن ایدی ربات پیام رسان سودو 
-	SudoUser = {226283662,378393503,378713184,311231963,502443907}, -- محل قرار دادن آيدي سودو هاي ربات
-	CliBotId = 378393503, -- محل قرار دادن آيدي ربات ضد تبليغات اصلي
-	Sudo = 378393503, -- محل قرار دادن آيدي سودوی اصلی ربات
-	ApiBotId = 302333716, -- محل قرار دادن آيدي ربات هلپر
-	ApiBotToken = '302333716:AAFa0CW7-Z4n6oGpaChBtH0QfQt_jqAniHU' -- محل قرار دادن توکن ربات هلپر
+	Channel_Link = 'Https://T.me/NIMA_FUCKER', --- محل قرار دادن لینک چنل 
+Sudo_UserName = 'Https://T.me/MR_N_I_M_A', --- محل قرار دادن ایدی سودو 
+Sodo_PmResan = 'Https://T.me/PvUnknownbot', --- محل قرار دادن ایدی ربات پیام رسان سودو 
+	SudoUser = {378373042}, -- محل قرار دادن آيدي سودو هاي ربات
+	CliBotId = 542054584, -- محل قرار دادن آيدي ربات ضد تبليغات اصلي
+	Sudo = 378373042, -- محل قرار دادن آيدي سودوی اصلی ربات
+	ApiBotId = 595920155, -- محل قرار دادن آيدي ربات هلپر
+	ApiBotToken = '354053714:AAEonGKz-CI3eG8W9VVR-vzxZ8UGhJvO3gM' -- محل قرار دادن توکن ربات هلپر
 }
 
 return config
